736cbd5fe1c469a8b27b43d6fb67b0c6b1c87086
d.kosenko <dimweb.tech@mail.ru>
2023-03-02 00:49:10 +0300

    enable mouth animation and stop control

--- src/App.vue
+++ src/App.vue
@@ -4,73 +4,64 @@ export default {
   data() {
     return {
       message: "Hello Vue!",
+      infinitySpeak: "",
+      pixiApp: "",
     };
   },
-  mounted() {
+  async mounted() {
     const cubism2Model =
       "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/shizuku/shizuku.model.json";
-    const cubism4Model =
-      "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/haru/haru_greeter_t03.model3.json";
+    // const cubism4Model =
+    //   "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/haru/haru_greeter_t03.model3.json";
 
     const live2d = PIXI.live2d;
 
-    (async function main() {
-      const app = new PIXI.Application({
-        view: document.getElementById("canvas"),
-        autoStart: true,
-        resizeTo: window,
-        backgroundColor: 0x333333,
-      });
+    const pixiApp = new PIXI.Application({
+      view: document.getElementById("canvas"),
+      autoStart: true,
+      resizeTo: window,
+      backgroundColor: 0x333333,
+    });
 
-      const models = await Promise.all([
-        live2d.Live2DModel.from(cubism2Model),
-        live2d.Live2DModel.from(cubism4Model),
-      ]);
+    this.pixiApp = pixiApp;
 
-      models.forEach((model) => {
-        app.stage.addChild(model);
+    const models = await Promise.all([
+      live2d.Live2DModel.from(cubism2Model),
+      // live2d.Live2DModel.from(cubism4Model),
+    ]);
 
-        const scaleX = (innerWidth * 0.3) / model.width;
-        const scaleY = (innerHeight * 0.5) / model.height;
+    models.forEach((model) => {
+      pixiApp.stage.addChild(model);
 
-        // fit the window
-        model.scale.set(Math.min(scaleX, scaleY));
+      const scaleX = (innerWidth * 0.3) / model.width;
+      const scaleY = (innerHeight * 0.5) / model.height;
 
-        model.y = innerHeight * 0.1;
+      // fit the window
+      model.scale.set(Math.min(scaleX, scaleY));
 
-        draggable(model);
-        addFrame(model);
-        addHitAreaFrames(model);
-      });
+      model.y = innerHeight * 0.1;
+    });
 
-      const model2 = models[0];
-      const model4 = models[1];
+    const model2 = models[0];
 
-      model2.x = (innerWidth - model2.width - model4.width) / 2;
-      model4.x = model2.x + model2.width;
+    let soundCounter = 0;
+    let infinitySpeak = (del) => {
+      // console.log(model2.internalModel.motionManager);
+      const animationGroup =
+        model2.internalModel.motionManager.state.currentGroup;
+      if (animationGroup == "idle") {
+        model2.motion("flick_head");
+      }
+    };
+    this.infinitySpeak = infinitySpeak;
+    pixiApp.ticker.add(infinitySpeak);
 
-      // handle tapping
-
-      model2.on("hit", (hitAreas) => {
-        if (hitAreas.includes("body")) {
-          model2.motion("tap_body");
-        }
-
-        if (hitAreas.includes("head")) {
-          model2.expression();
-        }
-      });
-
-      model4.on("hit", (hitAreas) => {
-        if (hitAreas.includes("Body")) {
-          model4.motion("Tap");
-        }
-
-        if (hitAreas.includes("Head")) {
-          model4.expression();
-        }
-      });
-    })();
+    model2.on("hit", (hitAreas) => {
+      if (hitAreas.includes("body")) {
+        console.log(model2.internalModel.motionManager);
+        model2.internalModel.motionManager.startMotion("flick_head", 0);
+      }
+    });
 
     function draggable(model) {
       model.buttonMode = true;
@@ -88,56 +79,17 @@ export default {
       model.on("pointerupoutside", () => (model.dragging = false));
       model.on("pointerup", () => (model.dragging = false));
     }
-
-    function addFrame(model) {
-      const foreground = PIXI.Sprite.from(PIXI.Texture.WHITE);
-      foreground.width = model.internalModel.width;
-      foreground.height = model.internalModel.height;
-      foreground.alpha = 0.2;
-
-      model.addChild(foreground);
-
-      checkbox("Model Frames", (checked) => (foreground.visible = checked));
-    }
-
-    function addHitAreaFrames(model) {
-      const hitAreaFrames = new live2d.HitAreaFrames();
-
-      model.addChild(hitAreaFrames);
-
-      checkbox(
-        "Hit Area Frames",
-        (checked) => (hitAreaFrames.visible = checked)
-      );
-    }
-
-    function checkbox(name, onChange) {
-      const id = name.replace(/\W/g, "").toLowerCase();
-
-      let checkbox = document.getElementById(id);
-
-      if (!checkbox) {
-        const p = document.createElement("p");
-        p.innerHTML = `<input type="checkbox" id="${id}"> <label for="${id}">${name}</label>`;
-
-        document.getElementById("control").appendChild(p);
-        checkbox = p.firstChild;
-      }
-
-      checkbox.addEventListener("change", () => {
-        onChange(checkbox.checked);
-      });
-
-      onChange(checkbox.checked);
-    }
+  },
+  methods: {
+    stopSpeak() {
+      this.pixiApp.ticker.remove(this.infinitySpeak);
+    },
   },
 };
 </script>
 
 <template>
-  <h1>Blank</h1>
+  <button @click="stopSpeak">Stop</button>
   <canvas id="canvas"></canvas>
   <div id="control"></div>
 </template>
-
-<style scoped></style>
