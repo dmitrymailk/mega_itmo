0d8fa2e5223b88a189bc284d01eadf9930eefa5a
dmitrymailk <dmitrymail_k@mail.ru>
2023-03-02 12:56:51 +0300

    acc 57

--- face_recognition_experiments/face_emotion/inference.py
+++ face_recognition_experiments/face_emotion/inference.py
@@ -1,39 +1,75 @@
-from face_emotion.train import LitModel
+from face_emotion.train import LitModel2
 
 import torch
 import torchvision
 from torchvision import transforms
 from PIL import Image
 
+# from keras.models import load_model
+
+
 if __name__ == "__main__":
-    model = LitModel.load_from_checkpoint(
-        "/home/dimweb/Desktop/sandbox/mega_itmo/face_recognition_experiments/wandb-lightning/4jxffekz/checkpoints/epoch=20-step=2121.ckpt"
-    )
-    emotions = {
-        0: "Angry",
-        1: "Disgust",
-        2: "Fear",
-        3: "Happy",
-        4: "Sad",
-        5: "Surprise",
-        6: "Neutral",
-    }
+    frame = "torch"
+    if frame == "torch":
+        model = LitModel2.load_from_checkpoint(
+            "/home/dimweb/Desktop/sandbox/mega_itmo/face_recognition_experiments/wandb-lightning/zrtotnhv/checkpoints/epoch=49-step=2550.ckpt"
+        )
+        model.eval()
 
-    model.eval()
-    image = Image.open(
-        "/home/dimweb/Desktop/sandbox/mega_itmo/face_recognition_experiments/img/image_3.png",
-    )
-    transform_image = transforms.Compose(
-        [
-            transforms.Grayscale(num_output_channels=3),
-            transforms.ToTensor(),
-        ]
-    )
-    image = transform_image(image)
-    image = image.unsqueeze(0)
+        emotions = {
+            0: "Angry",
+            1: "Disgust",
+            2: "Fear",
+            3: "Happy",
+            4: "Sad",
+            5: "Surprise",
+            6: "Neutral",
+        }
 
-    # predict with the model
-    print(image.shape)
-    y_hat = model(image)
-    pred = int(y_hat.argmax())
-    print(emotions[pred])
+        image = Image.open(
+            "/home/dimweb/Desktop/sandbox/mega_itmo/face_recognition_experiments/img/image_6.png",
+        )
+        transform_image = transforms.Compose(
+            [
+                transforms.Grayscale(num_output_channels=3),
+                transforms.ToTensor(),
+                transforms.Resize((32, 32)),
+            ]
+        )
+        image = transform_image(image)
+        image = image.unsqueeze(0)
+
+        # predict with the model
+        print(image.shape)
+        y_hat = model(image)
+        pred = int(y_hat.argmax())
+        print(emotions[pred])
+    else:
+        model = load_model(
+            "/home/dimweb/Desktop/sandbox/mega_itmo/face_recognition_experiments/top_model/epoch_75.hdf5"
+        )
+        emotions = {
+            0: "Angry",
+            1: "Disgust",
+            2: "Fear",
+            3: "Happy",
+            4: "Sad",
+            5: "Surprise",
+            6: "Neutral",
+        }
+
+        image = Image.open(
+            "/home/dimweb/Desktop/sandbox/mega_itmo/face_recognition_experiments/img/image_5.png",
+        )
+        transform_image = transforms.Compose(
+            [
+                transforms.ToTensor(),
+            ]
+        )
+        image = transform_image(image)
+
+        # predict with the model
+        print(image.shape)
+        y_hat = model.predict(image.numpy())
+        pred = int(y_hat.argmax())
+        print(emotions[pred])
--- face_recognition_experiments/face_emotion/train.py
+++ face_recognition_experiments/face_emotion/train.py
@@ -34,6 +34,7 @@ class FER2013DataModule(pl.LightningDataModule):
             [
                 transforms.Grayscale(num_output_channels=3),
                 transforms.ToTensor(),
+                transforms.Resize((32, 32)),
             ]
         )
 
@@ -173,13 +174,133 @@ class LitModel(pl.LightningModule):
         return optimizer
 
 
+class VGGNet(nn.Module):
+    def __init__(self, n_classes=None, conv_arch=None):
+        super(VGGNet, self).__init__()
+        self.conv_arch = conv_arch
+        self.n_classes = n_classes
+
+        self.conv = self.get_conv()
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            # The fully-connected part
+            nn.Linear(conv_arch[-1][1] * 4 * 4, 200),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.BatchNorm1d(200),
+            nn.Linear(200, 100),
+            nn.ReLU(),
+            # nn.Dropout(0.5),
+            nn.BatchNorm1d(100),
+            nn.Linear(100, n_classes),
+        )
+
+    def get_conv(self):
+        conv_blks = []
+        in_channels = 3
+        # The convolutional part
+        for num_convs, out_channels in self.conv_arch:
+            conv_blks.append(self.vgg_block(num_convs, in_channels, out_channels))
+            in_channels = out_channels
+
+        return nn.Sequential(*conv_blks)
+
+    def vgg_block(self, num_convs, in_channels, out_channels):
+        layers = []
+        for _ in range(num_convs):
+            layers.extend(
+                [
+                    nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                    nn.Dropout(0.5),
+                    nn.BatchNorm2d(out_channels),
+                    nn.ReLU(),
+                ]
+            )
+            in_channels = out_channels
+        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.classifier(x)
+        return x
+
+
+class LitModel2(pl.LightningModule):
+    def __init__(
+        self,
+        input_shape,
+        num_classes,
+        learning_rate=2e-4,
+    ):
+        super().__init__()
+
+        # log hyperparameters
+        self.save_hyperparameters()
+        self.learning_rate = learning_rate
+
+        conv_arch = ((2, 64), (2, 128), (3, 256))
+        self.model = VGGNet(num_classes, conv_arch=conv_arch)
+
+        self.accuracy = Accuracy("multiclass", num_classes=num_classes)
+
+    # will be used during inference
+    def forward(self, x):
+        x = self.model(x)
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+
+        # training metrics
+        preds = torch.argmax(logits, dim=1)
+        acc = self.accuracy(preds, y)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, logger=True)
+        self.log("train_acc", acc, on_step=True, on_epoch=True, logger=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+
+        # validation metrics
+        preds = torch.argmax(logits, dim=1)
+        acc = self.accuracy(preds, y)
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", acc, prog_bar=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+
+        # validation metrics
+        preds = torch.argmax(logits, dim=1)
+        acc = self.accuracy(preds, y)
+        self.log("test_loss", loss, prog_bar=True)
+        self.log("test_acc", acc, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+
+
 if __name__ == "__main__":
     dm = FER2013DataModule(
-        batch_size=256,
+        batch_size=512,
     )
     dm.prepare_data()
     dm.setup()
-    model = LitModel(
+    model = LitModel2(
         input_shape=(3, 32, 32),
         num_classes=dm.num_classes,
     )
@@ -198,7 +319,7 @@ if __name__ == "__main__":
 
     # Initialize a trainer
     trainer = pl.Trainer(
-        max_epochs=50,
+        max_epochs=80,
         gpus=1,
         logger=wandb_logger,
         callbacks=[
